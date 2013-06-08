dc1d5f5d8726fecb3a665722f2ae99034d08e124
Marvin Rounce <marvin.rounce@gmail.com>
2013-06-08 14:51:34 +0100

    Initail commit

---
+++ DropzoneMVCDemo/Controllers/FileUploadController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropzoneMVCDemo.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            //TODO Handle the uploaded file here
+            return View();
+        }
+    }
+}
---
+++ DropzoneMVCDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DropzoneMVCDemo.MvcApplication" Language="C#" %>
